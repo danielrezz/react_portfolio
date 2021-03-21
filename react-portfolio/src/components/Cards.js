590a5802664e2826c1b5e3ed6a3a9a171d91e633
Daniel Reza <daniel.reza1007@gmail.com>
2021-03-21 00:27:28 -0500

    pushing

---
+++ react-portfolio/src/components/Cards.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+    return (
+        <div className="cards">
+            <h1>Check out some of these projects</h1>
+            <div className="cards__container">
+                <div class="cards__wrapper">
+                    <ul className="cards__item">
+                        <CardItem 
+                        src="images/Screen Shot 2020-12-05 at 7.32.00 PM.png"
+                        text="Pokemon Team Builder"
+                        label="Pokemon"
+                        path="/work"
+                        />
+                        <CardItem 
+                        src="images/Screen Shot 2020-12-05 at 7.32.38 PM.png"
+                        text="Weather Dashboard"
+                        label="Weather"
+                        path="/work"
+                        />
+                        <CardItem 
+                        src="images/screenshot.PNG"
+                        text="QuizNode"
+                        label="QuizNode"
+                        path="/work"
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
---
+++ react-portfolio/src/components/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button } from './Button';
+import './Hero.css';
+import '../App.css';
+
+
+function Hero() {
+    return (
+        <div className='hero-container'>
+            <h1>Hi there</h1>
+            <p>Welcome to my portfolio site!</p>
+            <div className='hero-btns'>
+                <Button className='btns' buttonStyle='btn--outline'
+                buttonSize='btn--large'>
+                    Work
+                </Button>
+                <Button className='btns' buttonStyle='btn--primary'
+                buttonSize='btn--large'>
+                    Contact <i className='far fa-play-circle' />
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
